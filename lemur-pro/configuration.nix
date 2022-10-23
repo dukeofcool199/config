65a1bc6108eb8d4a43eda162174895133ddff709
Jenkin Schibel <jenkin.schibel@protonmail.com>
2022-10-22 21:29:07 -0700

    addd kvm intel kernel module

--- lemur-pro/configuration.nix
+++ lemur-pro/configuration.nix
@@ -14,6 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.kernelModules = [ "kvm-intel" ];
 
   networking.hostName = "skyberspace"; # Define your hostname.
 
