edfeb37430c07565dd89bcf7053cbf30922eac5b
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-01-04 15:54:03 -0800

    add autorandr support for lemur

--- lemur-pro/configuration.nix
+++ lemur-pro/configuration.nix
@@ -6,6 +6,8 @@
 
 let
   initialPassword = "";
+  eDP-1 = "00ffffffffffff000daed51400000000281a0104a51f117802ee95a3544c99260f505400000001010101010101010101010101010101b43b804a71383440503c680035ad10000018000000fe004e3134304843452d454e320a20000000fe00434d4e0a202020202020202020000000fe004e3134304843452d454e320a200010";
+  DP-1 = "00ffffffffffff0006b32b2701000000091f0103803c2278ae9315ae4e46a1260e5054a5cb0081c081809500a9c0b300d1c001010101565e00a0a0a029503020350055502100001a000000fd0030901ed83c000a202020202020000000ff004d324c4d44573031363938360a000000fc005647323757510a20202020202001d5020349f35301023f0405901113141f5e5f5d5e5f40061522230907078301000068030c002000382d0067d85dc401788003681a000001013090ede305e301e30f000ce60607016659289ee00078a0a032501040350055502100001a6fc200a0a0a055503020350055502100001a5aa000a0a0a046503020350055502100001aed";
 in
 {
   imports =
@@ -36,6 +38,45 @@ in
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
+
+  services.autorandr = {
+    enable = true;
+    profiles = {
+      "docked" = {
+        fingerprint = {
+          inherit eDP-1 DP-1;
+        };
+        config = {
+          DP-1 = {
+            enable = true;
+            crtc = 0;
+            primary = true;
+            position = "0x0";
+            mode = "2560x1440";
+            rate = "59.95";
+          };
+          eDP-1 = {
+            enable = false;
+          };
+        };
+      };
+      "normal" = {
+        fingerprint = {
+          inherit eDP-1;
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            primary = true;
+            position = "0x0";
+            mode = "1920x1080";
+            rate = "60.01";
+          };
+        };
+      };
+    };
+
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
