0a45aa800e026a506e3a80be970316b1a48ae3aa
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-03-17 11:11:49 -0700

    fixed tuner studio executable, also made it reference the correct jdk version

--- packages/tunerStudio/default.nix
+++ packages/tunerStudio/default.nix
@@ -3,18 +3,19 @@
 
 with pkgs;
 stdenv.mkDerivation {
-  name = "TunerStudio.sh";
-  pname = "tuner-studio";
+  name = "tuner-studio";
+  pname = "TunerStudio.sh";
   version = "3.1.08";
   src = fetchTarball
     {
       url = "https://www.tunerstudio.com/downloads2/TunerStudioMS_v3.1.08.tar.gz";
       sha256 = "13pin0fm8bsa35iba0pkl1x2bndwsgfp41pyyhs7xaaky6p865zs";
     };
-  buildInputs = [ jdk ];
+  buildInputs = [ sd jdk ];
   installPhase = ''
     mkdir -p $out/bin
     cp -r $src/* $out/bin
+    sd 'java' '${jdk}/bin/java' $out/bin/TunerStudio.sh
     chmod 777 $out/bin/TunerStudio.sh
   '';
 
