19d7ae42aab4f9cd4f7b0651128ab2eb2117c5ce
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-03-25 12:04:50 -0700

    added dev shell, added flake utility and git bug to shell to help with development

--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
       "github:nix-community/nixos-generators";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
+
     snowFallFlake.url = "github:snowfallorg/flake";
     snowFallFlake.inputs.nixpkgs.follows = "unstable";
 
@@ -21,22 +24,39 @@
   };
 
   outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
+    let
+      configuration =
+        inputs.snowfall-lib.mkFlake {
 
-      package-namespace = "jenkos";
+          package-namespace = "jenkos";
 
-      inherit inputs;
-      src = ./.;
+          inherit inputs;
+          src = ./.;
 
-      systems.hosts.skyberspace.modules = with inputs; [
-        nixos-hardware.nixosModules.system76
-      ];
+          systems.hosts.skyberspace.modules = with inputs; [
+            nixos-hardware.nixosModules.system76
+          ];
 
 
-      channels-config = {
-        allowUnfree = true;
-        allowInsecure = true;
-      };
+          channels-config = {
+            allowUnfree = true;
+            allowInsecure = true;
+          };
 
-    };
+        };
+
+      devShells = inputs.flake-utils.lib.eachDefaultSystem
+        (system:
+          let
+            pkgs = inputs.nixpkgs.legacyPackages.${system};
+          in
+          with pkgs;
+          {
+            devShells.default = mkShell
+              {
+                buildInputs = [ inputs.snowFallFlake.packages.x86_64-linux.default git-bug ];
+              };
+          });
+    in
+    inputs.nixpkgs.lib.recursiveUpdate configuration devShells;
 }
