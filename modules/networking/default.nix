43a5994ea4a09c5b1470b2fabe300e907e3a8081
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-04-23 12:56:16 -0700

    system/add browndog server configuration

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -3,13 +3,11 @@
 with lib;
 let
   cfg = config.jenkos.networking;
-  webPorts = optParams cfg.isWebServer [ 80 443 ] [ ];
 in
 {
   options.jenkos.networking = with types; {
     enable = mkBoolOpt true "enable networking?";
     enableFirewall = mkBoolOpt true "enable firewall?";
-    isWebServer = mkBoolOpt false "is this for server applications?";
     allowedTcpPorts = mkOpt (listOf number) [ ] "list of additional allowed TCP ports";
     allowedUdpPorts = mkOpt (listOf number) [ ] "list of additional allowed UDP ports";
 
@@ -29,7 +27,7 @@ in
         trustedInterfaces = [ "vboxnet0" ];
         enable = cfg.enableFirewall;
         allowPing = false;
-        allowedTCPPorts = webPorts ++ cfg.allowedTcpPorts;
+        allowedTCPPorts = cfg.allowedTcpPorts;
         allowedUDPPorts = cfg.allowedUdpPorts;
       };
 
---
+++ systems/x86_64-linux/browndog/default.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, inputs, lib, ... }:
+with lib;
+{
+  # imports = [ ./hardware.nix ];
+
+  jenkos = {
+    system = {
+      nix = enabled;
+      time = enabled;
+      localisation = enabled;
+      environment = enabled;
+      boot = enabled;
+    };
+    networking = {
+      enable = yes;
+      allowedTcpPorts = [ 80 443 ];
+    };
+    services = {
+      virtualisation = {
+        docker = yes;
+        docker-rootless = yes;
+        arion = yes;
+      };
+      ssh = {
+        openssh = enabled;
+      };
+      sftp = {
+        enable = yes;
+        users = [ "jenkin" ];
+      };
+      printing = enabled;
+      avahi = enabled;
+      gpg = enabled;
+      udisks = enabled;
+    };
+    developer = {
+      git = {
+        enable = yes;
+        github = yes;
+      };
+      vim = enabled;
+      nix = enabled;
+    };
+    users = {
+      jenkin = enabled;
+    };
+    utilities = {
+      networking = enabled;
+      browsing = {
+        cli = yes;
+      };
+      filemanager = {
+        tui = yes;
+      };
+      shelltools = enabled;
+      sysadmin = enabled;
+      filecopy = enabled;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
+
