779a3c541be7476fd9e690c4cc7111e253aa98ea
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-04-23 17:28:52 -0700

    system/browndog added hardware configuration

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -14,6 +14,7 @@ in
         systemd-boot.enable = true;
         systemd-boot.configurationLimit = 10;
         efi.canTouchEfiVariables = true;
+        efi.efiSysMountPoint = "/boot/efi";
         # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
         systemd-boot.editor = false;
 
--- systems/x86_64-linux/browndog/default.nix
+++ systems/x86_64-linux/browndog/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, lib, ... }:
 with lib;
 {
-  # imports = [ ./hardware.nix ];
+  imports = [ ./hardware.nix ];
 
   jenkos = {
     system = {
