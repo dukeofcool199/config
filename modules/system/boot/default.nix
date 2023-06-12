a66829f3c0ace6403b20a7608c41214c768ef85e
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-06-12 16:48:57 -0700

    boot options?

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -15,7 +15,7 @@ in
         systemd-boot.enable = true;
         systemd-boot.configurationLimit = 35;
         efi.canTouchEfiVariables = cfg.efi;
-        efi.efiSysMountPoint = if cfg.efi then "/boot/efi" else "";
+        efi.efiSysMountPoint = if cfg.efi then "/boot/efi" else "/boot";
 
         # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
         systemd-boot.editor = false;
