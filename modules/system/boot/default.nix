4be6de071e1025fd01037d15c65d317de94dc91f
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-06-12 16:24:45 -0700

    add option for efi booting in boot module

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -6,6 +6,7 @@ in
 {
   options.jenkos.system.boot = with types; {
     enable = mkBoolOpt false "common boot options?";
+    efi = mkBoolOpt true "booted with efi?";
   };
 
   config = mkIf cfg.enable {
@@ -13,8 +14,8 @@ in
       loader = {
         systemd-boot.enable = true;
         systemd-boot.configurationLimit = 35;
-        efi.canTouchEfiVariables = true;
-        efi.efiSysMountPoint = "/boot/efi";
+        efi.canTouchEfiVariables = cfg.efi;
+        efi.efiSysMountPoint = if cfg.efi then "/boot/efi" else null;
         # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
         systemd-boot.editor = false;
 
