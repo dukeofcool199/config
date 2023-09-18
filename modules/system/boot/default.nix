c015d7a7789b0362a8272ce546503bec5eb8bc3c
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-09-17 19:40:46 -0600

    more grub config for boot

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -6,21 +6,34 @@ in
 {
   options.jenkos.system.boot = with types; {
     enable = mkBoolOpt false "common boot options?";
+    grub = mkBoolOpt false "grub?";
     efi = mkBoolOpt true "booted with efi?";
   };
 
   config = mkIf cfg.enable {
     boot = {
-      loader = {
-        systemd-boot.enable = true;
-        systemd-boot.configurationLimit = 35;
-        efi.canTouchEfiVariables = cfg.efi;
-        efi.efiSysMountPoint = if cfg.efi then "/boot/efi" else "/boot";
+      loader =
+        if cfg.grub then {
+          efi =
+            if cfg.efi then {
+              canTouchEfiVariables = true;
+              efiSysMountPoint = "/boot/efi";
+            } else { };
+          grub = {
+            efiSupport = true;
+            efiInstallAsRemovable = true;
+            device = "nodev";
+          };
+        } else {
+          systemd-boot.enable = true;
+          systemd-boot.configurationLimit = 35;
+          efi.canTouchEfiVariables = cfg.efi;
+          efi.efiSysMountPoint = if cfg.efi then "/boot/efi" else "/boot";
 
-        # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
-        systemd-boot.editor = false;
+          # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
+          systemd-boot.editor = false;
 
-      };
+        };
     };
 
 
