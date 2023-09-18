c26d8b6dc5f5dde9e41d50b9b95d4772172a4f8a
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-09-17 19:40:35 -0600

    update ammobox hardware file

--- systems/x86_64-linux/ammobox/hardware.nix
+++ systems/x86_64-linux/ammobox/hardware.nix
@@ -5,39 +5,37 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "uas" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/966d3736-3faa-4a9d-b764-f472881f4d0f";
+    { device = "/dev/disk/by-uuid/11b8930f-6f70-4ef7-aa1a-e71c68cc85b7";
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
-    {
-      device = "/dev/disk/by-uuid/2465-E55F";
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/D0FD-ABF6";
       fsType = "vfat";
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/f5eb087e-0585-4eab-8b5d-8982eeb72f70"; }];
+    [ { device = "/dev/disk/by-uuid/22bc0370-38d1-4823-aebe-61b54dbe7c28"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp34s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
 }
