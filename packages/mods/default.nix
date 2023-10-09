570711342e15f1b0759a9fbb13912f9137f45e9f
Jenkin Schibel <jenkin.schibel@protonmail.com>
2023-10-09 17:44:46 -0600

    update mods

--- packages/mods/default.nix
+++ packages/mods/default.nix
@@ -2,15 +2,16 @@
 
 buildGoModule rec {
   pname = "mods";
-  version = "0.2.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-jOvXT/KAfSN9E4ZgntCbTu05VJu1jhGtv6gEgLStd98=";
+    sha256 = "sha256-0PykG19DAVVziRAefQ0LENMuTfTJSVFvs0bMJXdDkrE=";
   };
 
-  vendorHash = "sha256-GNGX8dyTtzRSUznEV/do1H7GEf6nYf0w+CLCZfkktfg=";
+  vendorSha256 = "sha256-DfIXW5cfTnXPgl9IC5+wTFJ04qWX4RlVoDIYM5gooks=";
+
 
 }
